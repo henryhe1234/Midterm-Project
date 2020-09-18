494168e5c45514b8f6ea7ec78f6675f0c748d158
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-18 16:25:24 -0700

    todosRoutes(db) added

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const widgetsRoutes = require("./routes/widgets");
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/todos", todosRoutes);
+app.use("/todos", todosRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
