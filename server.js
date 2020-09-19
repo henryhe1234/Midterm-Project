570e28cc88c6d029376b8f48d70473ba460bfffa
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-19 16:41:16 -0700

    server handles all the routes

--- server.js
+++ server.js
@@ -35,13 +35,32 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+
+// Todo Routes
 const todosRoutes = require("./routes/todos");
+const todoNewRoute = require("./routes/todo_new");
+const todoEditRoute = require("./routes/todo_edit");
+const todoDeleteRoute = require("./routes/todo_delete");
+
+
+// Login && Register Routes
+
+const login = require("./routes/login");
+const register = require("./routes/register");
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use("/todos", todosRoutes);
+app.use("/todo/new", todoNewRoute);
+app.use("/todo/:id/edit", todoEditRoute);
+app.use("/todo/:id/delete", todoDeleteRoute);
+app.use("/login", login);
+app.use("/register", register);
+
+
 // Note: mount other resources here, using the same pattern above
 
 
