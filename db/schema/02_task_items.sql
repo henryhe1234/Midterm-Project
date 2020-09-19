e7bf0204506f767cec974dc9586e724fe4013702
henryhe1234 <henryhe1123@gmail.com>
2020-09-19 23:35:26 +0000

    refactor the database design

--- db/schema/02_task_items.sql
+++ db/schema/02_task_items.sql
@@ -2,9 +2,14 @@ DROP TABLE IF EXISTS task_items CASCADE;
 
 CREATE TABLE task_items(
   id SERIAL PRIMARY KEY NOT NULL,
-  info TEXT NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  title VARCHAR(255) NOT NULL,
+  create_on DATE NOT NULL,
   category VARCHAR(255) NOT NULL,
   scheduled_date DATE NOT NULL,
   completed_date DATE NOT NULL,
+
+  info TEXT NOT NULL,
   is_active BOOLEAN NOT NULL DEFAULT TRUE
+
 );
