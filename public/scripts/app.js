3bd28c330681c6022d6da776de9f05997a0afc5f
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-19 16:15:26 -0700

    app.js not getting any users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,7 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for (const user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
+    console.log(users);
   });
 
 });
