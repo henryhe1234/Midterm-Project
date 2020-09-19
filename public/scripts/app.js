546ea265e2136a5613fce9fb02cbbef4eaf53e12
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-19 14:43:50 -0700

    todos api get request created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,8 +3,16 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {
+    for (const user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+  $.ajax({
+    method: "GET",
+    url:"/api/todos"
+  }).done((todos) => {
+    for (const todo of todos) {
+      $("<h1>").text(todo).appendTo($("body"));
+    }
+  });
 });
