8a26105078ee8ea0bf8abfdf57f986034a6a008e
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-19 16:16:03 -0700

    passes in variables

--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 const data = require("./fake_data/todos.json");
-
 router.get("/", (req, res) => {
 
-  const templateVars = data;
-
+  const templateVars = { todo: data.task_lists };
   console.log(templateVars);
   res.render("todos", templateVars);
 });
