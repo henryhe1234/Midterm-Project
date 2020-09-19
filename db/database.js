6b876e0823a98e7f5514064b7fde2d5865a92085
henryhe1234 <henryhe1123@gmail.com>
2020-09-19 23:12:25 +0000

    modify a field of task_items, changing name to info, also implement four function to add to task_items

--- db/database.js
+++ db/database.js
@@ -1,9 +1,9 @@
 const { Pool, Query } = require('pg');
 const pool = new Pool({
-  user:'labber',
-  password:'labber',
-  host:'localhost',
-  database:'midterm'
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
 });
 
 // DB_HOST=localhost
@@ -21,12 +21,68 @@ const addUser = (user) => {
     .then((res) => {
       return res.rows[0];
     })
+};
+
+// INSERT INTO task_items (name, category,scheduled_date,completed_date) VALUES ('mcdonalds', 'restaurants','2018-07-20','2019-08-03');
+const addBooks = (book, scheduled_date, completed_date) => {
+  return pool.query(`
+  INSERT INTO task_items (
+    info,category,scheduled_date,completed_date)
+    VALUES($1,'books',$2,$3)
+    RETURNING *;
+`, [book, scheduled_date, completed_date])
+    .then((res) => {
+      return res.rows[0];
+    })
 }
+const addMovie = (movie, scheduled_date, completed_date) => {
+  return pool.query(`
+  INSERT INTO task_items (
+    info,category,scheduled_date,completed_date)
+    VALUES($1,'movie',$2,$3)
+    RETURNING *;
+`, [movie, scheduled_date, completed_date])
+    .then((res) => {
+      return res.rows[0];
+    })
+};
+const addRestaurant = (restaurant, scheduled_date, completed_date) => {
+  return pool.query(`
+  INSERT INTO task_items (
+    info,category,scheduled_date,completed_date)
+    VALUES($1,'restaurant',$2,$3)
+    RETURNING *;
+`, [restaurant, scheduled_date, completed_date])
+    .then((res) => {
+      return res.rows[0];
+    })
+};
+const addProduct = (product, scheduled_date, completed_date) => {
+  return pool.query(`
+  INSERT INTO task_items (
+    info,category,scheduled_date,completed_date)
+    VALUES($1,'product',$2,$3)
+    RETURNING *;
+`, [product, scheduled_date, completed_date])
+    .then((res) => {
+      return res.rows[0];
+    })
+};
+
 
 let user1 = {
-name:"Henry",
-email:"abc@abc",
-password:"1239494"
+  name: "Henry",
+  email: "abc@abc",
+  password: "1239494"
 }
-
-addUser(user1);
+let product = "this is just a product";
+let books = "this is just a book";
+let movie = "this is just a movie";
+let restaurant = "this is just a restaurant";
+let scheduled_date = "2020-08-19";
+let completed_date = "2020-03-18";
+addProduct(product,scheduled_date,completed_date);
+// addRestaurant(restaurant, scheduled_date, completed_date);
+// addMovie(movie,scheduled_date,completed_date);
+// addBooks(books,scheduled_date,completed_date);
+// addUser(user1);
--- db/seeds/02_task_items.sql
+++ db/seeds/02_task_items.sql
@@ -1,6 +1,6 @@
 -- -- Widgets table seeds here (Example)
-INSERT INTO task_items (name, category,scheduled_date,completed_date) VALUES ('Harry Porter','books','2019-10-22','2020-03-11');
-INSERT INTO task_items (name, category,scheduled_date,completed_date) VALUES ('mcdonalds', 'restaurants','2018-07-20','2019-08-03');
+INSERT INTO task_items (info, category,scheduled_date,completed_date) VALUES ('Harry Porter','book','2019-10-22','2020-03-11');
+INSERT INTO task_items (info, category,scheduled_date,completed_date) VALUES ('mcdonalds', 'restaurant','2018-07-20','2019-08-03');
 
 
 
