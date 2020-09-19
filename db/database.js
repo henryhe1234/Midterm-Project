070cedf53a8b8abf187cb7c9c32b40a488488fa2
henryhe1234 <henryhe1123@gmail.com>
2020-09-19 22:37:51 +0000

    create addUser file

--- db/database.js
+++ db/database.js
@@ -1,9 +1,32 @@
-const {Pool} = reuire('pg');
+const { Pool, Query } = require('pg');
+const pool = new Pool({
+  user:'labber',
+  password:'labber',
+  host:'localhost',
+  database:'midterm'
+});
 
-const addBook = ()=>{
-  return Pool.query(`
-  INSERT INTO task_items (
-    category,
-  )
-  `)
+// DB_HOST=localhost
+// DB_USER=labber
+// DB_PASS=labber
+// DB_NAME=midterm
+const addUser = (user) => {
+  return pool.query(`
+  INSERT INTO users (
+    name, email, password)
+    VALUES (
+    $1, $2, $3)
+    RETURNING *;
+  `, [user.name, user.email, user.password])
+    .then((res) => {
+      return res.rows[0];
+    })
 }
+
+let user1 = {
+name:"Henry",
+email:"abc@abc",
+password:"1239494"
+}
+
+addUser(user1);
