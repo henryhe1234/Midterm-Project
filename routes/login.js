dc4629d474eb3a592cf12c5c6bfc571a89e11b85
Ibirn <kmcricciuti@gmail.com>
2020-09-21 17:05:51 +0000

    Add simple cookie setting

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-router.post("/login", (req, res) => {
-  res.redirect("todos");
+router.get('/:id', (req, res) => {
+  req.session.id = req.params.id;
+  res.redirect('/');
 });
+
 module.exports = router;
