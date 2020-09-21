0fdce0fd77032b85bdd6651c94511a02d654405a
henryhe1234 <henryhe1123@gmail.com>
2020-09-21 18:47:11 +0000

    small changes

--- routes/todos.js
+++ routes/todos.js
@@ -4,12 +4,18 @@ const router  = express.Router();
 const taskSort = require("../lib/taskSort");
 
 router.get("/", (req, res) => {
+
+
   res.render("todos");
 });
 router.post("/", (req, res) => {
+
+
   const text = req.body["new-todo"];
   taskSort(text);
   res.redirect("/todos");
 });
+
+
 
 module.exports = router;
