eed66d0a4a8ce84998d426fcbe1d9ff0d6c2fd73
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-09-21 17:30:37 -0700

    Some changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,36 +6,4 @@ $(() => {
   }).done((users) => {
     console.log(users);
   });
-  const loadTodos = () => {
-    $.ajax({
-      method: "GET",
-      url: "/todos",
-      success: (data) => {
-        renderTodos(data);
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
-  };
-
-  const createNewTodo = (todo) => {
-    const $todo = $('<div class="todo">');
-
-    const $title = $('<h1>').text(todo);
-
-    $todo.append($title);
-  };
-
-  const renderTodos = (todos) => {
-    const $todosDiv = $(".todos");
-
-    $todosDiv.empty();
-
-    for (const id in todos) {
-      const todo = todos[id];
-      const $todo = createNewTodo(todo);
-      $todosDiv.prepend($todo);
-    }
-  };
 });
