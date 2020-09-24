a26c42a8c7c0d7105edba7bc9a70ce8ef547c4a7
Ibirn <kmcricciuti@gmail.com>
2020-09-24 18:50:16 +0000

    Add fucntion to create hidden div with json info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,25 +21,52 @@ $(() => {
       }
     );
   };
+
   const renderTodos = (todos) => {
     const $lists = $('.lists');
     $lists.empty();
     for (const item in todos) {
       const todo = todos[item];
-
       if (todo.category === "movie") {
         const $movie = $('.movie');
-        const $content = $('<p>').text(todo.title);
+        data = JSON.parse(todo["info"])
+        console.log(todo)
+        const $content = $(`
+        <p>${data["Title"]}</p>
+        <div class="popout">
+          <p>Year: ${data["Genre"]}</p>
+          <p>Rated: ${data["Rated"]}</p>
+          <p>Actors: ${data["Actors"]}</p>
+          <p>Reviews: ${data["imdbRating"]}</p>
+      </div>`)
+
         $movie.prepend($content);
       }
       if (todo.category === "restaurant") {
         const $food = $('.food');
-        const $content = $('<p>').text(todo.title);
+        data = JSON.parse(todo["info"])
+        console.log(JSON.parse(todo["info"]))
+        const $content = $(`<p>${data["name"]}</p>
+        <div class='popout'>
+          <p>Cuisine: ${data["categories"][0]["title"]}</p>
+          <p>Address: ${data["location"]["display_address"][0]}\n${data["location"]["display_address"][1]}</p>
+          <p></p>
+          <p>Price: ${data["price"]}</p>
+        </div>`);
         $food.prepend($content);
       }
       if (todo.category === "book") {
         const $book = $('.book');
-        const $content = $('<p>').text(todo.title);
+        console.log(JSON.parse(todo["info"]))
+        data = JSON.parse(todo["info"])
+        const $content = $(`
+        <p>${data["title"]}</p>
+        <div class='popout'>
+          <p>Authors: ${data["authors"][0]}</p>
+          <p>Genre: ${data["categories"][0]}</p>
+          <p>Publisher: ${data["publisher"]}</p>
+          <p>Rating: ${data["averageRating"]}</p>
+        </div>`);
         $book.prepend($content);
       }
       if (todo.category === "product ") {
@@ -48,8 +75,8 @@ $(() => {
         $product.prepend($content);
       }
     }
-
   };
+
   const $newTodo = $('#new-todo');
 
   $newTodo.on('submit', function(event) {
