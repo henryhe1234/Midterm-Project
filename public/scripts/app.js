444621c9aac2a8d0b1eff6d48c9e8181a2b6ef14
Ibirn <kmcricciuti@gmail.com>
2020-09-24 21:25:06 +0000

    Add modal information windows

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,34 +25,70 @@ $(() => {
   const renderTodos = (todos) => {
     const $lists = $('.lists');
     $lists.empty();
+    console.log("TODOS", todos)
     for (const item in todos) {
+      console.log("ITEM", item)
       const todo = todos[item];
       if (todo.category === "movie") {
         const $movie = $('.movie');
         data = JSON.parse(todo["info"])
         console.log(todo)
         const $content = $(`
-        <p>${data["Title"]}</p>
-        <div class="popout">
-          <p>Year: ${data["Genre"]}</p>
-          <p>Rated: ${data["Rated"]}</p>
-          <p>Actors: ${data["Actors"]}</p>
-          <p>Reviews: ${data["imdbRating"]}</p>
-      </div>`)
-
+        <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#movie${item}">
+        ${data["Title"]}</button></p>
+    <div class="modal fade" id="movie${item}" tabindex="-1" role="dialog" aria-labelledby="movieLabel${item}"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">${data["Title"]}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Year: ${data["Genre"]}</p>
+            <p>Rated: ${data["Rated"]}</p>
+            <p>Actors: ${data["Actors"]}</p>
+            <p>Reviews: ${data["imdbRating"]}</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+`)
         $movie.prepend($content);
       }
       if (todo.category === "restaurant") {
         const $food = $('.food');
         data = JSON.parse(todo["info"])
         console.log(JSON.parse(todo["info"]))
-        const $content = $(`<p>${data["name"]}</p>
-        <div class='popout'>
-          <p>Cuisine: ${data["categories"][0]["title"]}</p>
-          <p>Address: ${data["location"]["display_address"][0]}\n${data["location"]["display_address"][1]}</p>
-          <p></p>
-          <p>Price: ${data["price"]}</p>
-        </div>`);
+        const $content = $(`
+        <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#food${item}">
+    ${data["name"]}</button></p>
+<div class="modal fade" id="food${item}" tabindex="-1" role="dialog" aria-labelledby="foodLabel${item}"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${data["name"]}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <p>Cuisine: ${data["categories"][0]["title"]}</p>
+      <p>Address: ${data["location"]["display_address"][0]}\n${data["location"]["display_address"][1]}</p>
+      <p></p>
+      <p>Price: ${data["price"]}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+        `);
         $food.prepend($content);
       }
       if (todo.category === "book") {
@@ -60,18 +96,54 @@ $(() => {
         console.log(JSON.parse(todo["info"]))
         data = JSON.parse(todo["info"])
         const $content = $(`
-        <p>${data["title"]}</p>
-        <div class='popout'>
-          <p>Authors: ${data["authors"][0]}</p>
-          <p>Genre: ${data["categories"][0]}</p>
-          <p>Publisher: ${data["publisher"]}</p>
-          <p>Rating: ${data["averageRating"]}</p>
-        </div>`);
+        <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#book${item}">
+    ${data["title"]}</button></p>
+<div class="modal fade" id="book${item}" tabindex="-1" role="dialog" aria-labelledby="bookLabel${item}"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${data["title"]}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <p>Authors: ${data["authors"][0]}</p>
+      <p>Genre: ${data["categories"][0]}</p>
+      <p>Publisher: ${data["publisher"]}</p>
+      <p>Rating: ${data["averageRating"]} / 5</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>`);
         $book.prepend($content);
       }
       if (todo.category === "product ") {
         const $product = $('.product');
-        const $content = $('<p>').text(todo.title);
+        const $content = $(`
+        <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#product${item}">
+    ${elem["title"]}</button></p>
+<div class="modal fade" id="product${item}" tabindex="-1" role="dialog" aria-labelledby="productLabel${item}"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${elem["title"]}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <p>Find on Amazon: <a href=https://www.amazon.ca/s?k=${elem["title"]}>${elem["title"]}</a>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>`);
         $product.prepend($content);
       }
     }
@@ -89,19 +161,11 @@ $(() => {
         $(this).children('input').val('');
       });
   });
-  // $('#new-todo').on('submit', function(event) {
-  //   event.preventDefault();
-  //   const serializedData = $(this).serialize();
-  //   $.post("/todos", serializedData) //returns promise.
-  //     .then((res) => {
-  //       console.log("HERERES:", res);
-  //     });
-  // });
-  $('#loginform').on('submit', function(event){
-    event.preventDefault();
-    $(this).css("display","none")
-    $('#navbarSupportedContent > form').css("display", "block")
-  })
+
+  // $('#navbarSupportedContent > li').submit(function(event) {
+  //   event.preventDefault()
+  //   $(this).hide()
+  // })
 
   loadTodos();
 });
